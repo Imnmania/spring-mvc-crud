ccb6f66b6bdd8b93902c7a5fd4df672c24e4adfb
Imnmania <niloyb@live.com>
2023-03-19 23:10:48 +0600

    project running

---
+++ src/main/java/me/niloybiswas/customer/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package me.niloybiswas.customer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CustomerController {
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("message", "Hello from Spring MVC");
+		return mav;
+	}
+
+}
